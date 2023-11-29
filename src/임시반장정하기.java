0b185e3b4a2674a5418876d2e0b2d85004f291fa
rlaalsrl715 <minkik715@gmail.com>
2023-11-29 21:11:31 +0900

    2-9 끝

--- src/임시반장정하기.java
+++ src/임시반장정하기.java
@@ -1,14 +1,15 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.List;
 
 public class 임시반장정하기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[][] arrs = new int[5][n];
-        for (int i = 0; i < 5; i++) {
-            int[] arr = new int[n];
+        int[][] arrs = new int[n][5];
+        for (int i = 0; i < n; i++) {
+            int[] arr = new int[5];
             int j = 0;
             for (String str : br.readLine().split(" ")) {
                 arr[j++] = Integer.parseInt(str);
@@ -23,13 +24,16 @@ public class 임시반장정하기 {
 
     private static String solution(int n, int[][] arrs) {
         int[] answer = new int[n];
+        int[][] check = new int[n+1][n+1];
         for(int i=0; i<5; i++){
             for(int j=0; j<n-1; j++){
                 int temp = arrs[j][i];
                 for(int k =j+1; k<n; k++){
-                    if(temp == arrs[k][i]){
+                    if(temp == arrs[k][i] && check[k][j] == 0){
                         answer[j] = answer[j] +1;
                         answer[k] = answer[k] +1;
+                        check[k][j] =1;
+                        check[j][k] =1;
                     }
                 }
             }

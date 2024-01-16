dc248957de885eb63da76429d774c0b6f9f4da7e
rlaalsrl715 <minkik715@gmail.com>
2024-01-16 21:27:05 +0900

    sql practice

---
+++ sql/조건에부합하는중고거래상태조회하기.sql
@@ -0,0 +1,13 @@
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
+       case
+           when STATUS="SALE"
+               then "판매중"
+           when STATUS="RESERVED"
+               then "예약중"
+           when STATUS="DONE"
+               then "거래완료"
+           else ""
+           end as STATUS
+from USED_GOODS_BOARD
+where CREATED_DATE = "2022-10-05"
+order by BOARD_ID DESC;

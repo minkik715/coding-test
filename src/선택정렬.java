fe0eca1beadea22f3033d88e8a2b5af3041a4c51
rlaalsrl715 <minkik715@gmail.com>
2024-01-26 21:11:41 +0900

    6-1

---
+++ src/선택정렬.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class 선택정렬 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        br.readLine();
+        List<Integer> nums = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        bw.write(solution(nums));
+        bw.flush();
+
+    }
+
+    private static String solution(List<Integer> nums) {
+        int size = nums.size();
+        int lp, min, rLoc;
+        for (int i = 0; i < size-1; i++) {
+            lp = nums.get(i);
+            min = lp;
+            rLoc = i;
+            for (int j = i + 1; j < size; j++) {
+                int rp = nums.get(j);
+                if (min > rp) {
+                    min = rp;
+                    rLoc = j;
+                }
+            }
+            nums.set(i, min);
+            nums.set(rLoc, lp);
+        }
+        StringBuffer sb = new StringBuffer();
+        for(Integer num :nums){
+            sb.append(num).append(" ");
+        }
+        return sb.toString();
+    }
+}

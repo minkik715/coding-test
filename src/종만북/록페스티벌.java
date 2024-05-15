b2eb993393ada00598425c9f29f1764e1aecaaff
rlaalsrl715 <minkik715@gmail.com>
2024-05-15 13:49:32 +0900

    종만북: 록 페스티벌

--- src/종만북/록페스티벌.java
+++ src/종만북/록페스티벌.java
@@ -2,7 +2,6 @@ package 종만북;
 
 import java.io.*;
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class 록페스티벌 {
 
@@ -11,23 +10,43 @@ public class 록페스티벌 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] s = br.readLine().split(" ");
-        int costSize = Integer.parseInt(s[0]);
-        int teamSize = Integer.parseInt(s[1]);
-        int[] costs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        bw.write(String.valueOf(solution(costs, teamSize)));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+            int costSize = Integer.parseInt(s[0]);
+            int teamSize = Integer.parseInt(s[1]);
+            int[] costs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            bw.write(String.format("%.12f",solution(costs, teamSize)) + "\n");
+        }
         bw.flush();
         bw.close();
     }
 
-    private static Float solution(int[] costs, int team) {
+    private static Double solution(int[] costs, int team) {
+        double min = Double.MAX_VALUE;
+
+        for (int start = 0; start < costs.length; start++) {
+            double sum = 0;
+            for (int target = start; target < costs.length; target++) {
+                sum += costs[target];
+                int cnt = target - start +1;
+                if(cnt >= team){
+                    min = Math.min(min, sum/ cnt);
+                }
+            }
+        }
+        return min;
+    }
+
+    //오답 ... break의 문제 애초에 잘못됨
+    /*private static Float solution(int[] costs, int team) {
         float min = Float.MAX_VALUE;
 
         for (int start = 0; start < costs.length - 1; start++) {
             float sum = 0;
             int cnt = 0;
             float avg = Float.MAX_VALUE;
-            for (int target = start ; target < costs.length; target++) {
+            for (int target = start; target < costs.length; target++) {
                 if (cnt++ < team || avg > (sum + costs[target]) / cnt) {
                     sum += costs[target];
                     avg = sum / cnt;
@@ -38,5 +57,5 @@ public class 록페스티벌 {
             }
         }
         return min;
-    }
+    }*/
 }

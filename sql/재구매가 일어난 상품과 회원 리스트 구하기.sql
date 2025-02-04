aee207f7c019e666f52b37229c9f49e1df28f252
minki <minkik715@gmail.com>
2025-02-04 22:32:57 +0900

    sql 다시해볼까

--- sql/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ sql/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,5 +1,10 @@
--- 코드를 입력하세요
 SELECT USER_ID, PRODUCT_ID  FROM ONLINE_SALE
 GROUP BY USER_ID, PRODUCT_ID
 HAVING COUNT(*) >= 2
-ORDER BY 1 ASC, 2 DESC
+ORDER BY 1 ASC, 2 DESC;
+
+
+select t.user_id, t.product_id
+from( select user_id, product_id, count(*) as cnt from ONLINE_SALE group by user_id, product_id)  as t
+where t.cnt > 1
+order by t.user_id ASC, t.product_id DESC;
